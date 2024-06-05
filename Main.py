08cf4658f5ee05987a2308643cd977c75f92a2b2
JasonM430 <144463655+JasonM430@users.noreply.github.com>
2024-06-05 19:54:19 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -147,7 +147,7 @@ def main():
 
 
           events = ['nothing', 'item', 'fight', 'boss_fight', 'boss_fight_v2', 'boss_fight_v3' , 'trap']
-          probabilities = [0.1, 0.295, 0.2, 0.15, 0.05, 0.005, 0.2]  
+          probabilities = [0, 0, 0, 0, 1, 0, 0]  
 
           st.session_state.floor += 1
           event = random.choices(events, probabilities, k=1)[0]
